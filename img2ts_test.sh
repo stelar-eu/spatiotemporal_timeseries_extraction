455f3a3c9ec47c286950469474691124f92d890c
Jens d'Hondt <j.e.dhondt@student.tue.nl>
2024-03-06 14:44:43 +0100

    updated bash files

--- img2ts_test.sh
+++ img2ts_test.sh
@@ -4,17 +4,19 @@ docker pull alexdarancio7/stelar_image2ts:latest
 
 MINIO_ACCESS_KEY=minioadmin
 MINIO_SECRET_KEY=minioadmin
-MINIO_ENDPOINT_URL=http://localhost:9000
+MINIO_ENDPOINT_URL=localhost:9000
 
-ras_paths="s3://stelar-spatiotemporal/LAI/30TYQ_LAI_2020.RAS"
-rhd_paths="s3://stelar-spatiotemporal/LAI/30TYQ_LAI_2020.RHD"
-out_dir="s3://stelar-spatiotemporal/LAI"
-fields_path="s3://stelar-spatiotemporal/fields.gpkg"
+ras_paths="s3://$MINIO_ENDPOINT_URL/stelar-spatiotemporal/LAI/30TYQ_LAI_2020.RAS"
+rhd_paths="s3://$MINIO_ENDPOINT_URL/stelar-spatiotemporal/LAI/30TYQ_LAI_2020.RHD"
+out_dir="s3://$MINIO_ENDPOINT_URL/stelar-spatiotemporal/LAI"
+fields_path="s3://$MINIO_ENDPOINT_URL/stelar-spatiotemporal/fields.gpkg"
 
 docker run -it \
--e MINIO_ACCESS_KEY=$MINIO_ACCESS_KEY \
--e MINIO_SECRET_KEY=$MINIO_SECRET_KEY \
--e MINIO_ENDPOINT_URL=$MINIO_ENDPOINT_URL \
 --network="host" \
 alexdarancio7/stelar_image2ts \
-$ras_paths $rhd_paths $out_dir $fields_path -skipfields
+--input_path s3://localhost:9000/path/to/input_dir \
+--output_path s3://localhost:9000/path/to/output_dir \
+--field_path s3://localhost:9000/path/to/fields.shp \
+--skippx \
+--MINIO_ACCESS_KEY $MINIO_ACCESS_KEY \
+--MINIO_SECRET_KEY $MINIO_SECRET_KEY 
--- publish_docker.sh
+++ publish_docker.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker login
+echo "Please enter the password for the docker hub account alexdarancio7: "
+read -s DOCKER_PASSWORD
+
+docker login -u alexdarancio7 -p $DOCKER_PASSWORD
 
 docker build -t alexdarancio7/stelar_image2ts:latest .
 
