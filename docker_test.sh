9f2d87dac85702452ca3cc8f7210029e1a92c625
Jens d'Hondt <j.e.dhondt@student.tue.nl>
2024-04-16 16:53:01 +0200

    Added support for TIF files as input

--- docker_test.sh
+++ docker_test.sh
@@ -8,13 +8,14 @@ MINIO_ENDPOINT_URL=http://localhost:9000
 
 input_path="s3://stelar-spatiotemporal/LAI_small"
 output_path="s3://stelar-spatiotemporal"
-# fields_path="s3://stelar-spatiotemporal/fields.gpkg"
+fields_path="s3://stelar-spatiotemporal/fields_2020_07_27.gpkg"
 
 docker run -it \
 --network="host" \
 alexdarancio7/stelar_image2ts \
 --input_path $input_path \
 --output_path $output_path \
+--field_path $fields_path \
 --MINIO_ACCESS_KEY $MINIO_ACCESS_KEY \
 --MINIO_SECRET_KEY $MINIO_SECRET_KEY  \
 --MINIO_ENDPOINT_URL $MINIO_ENDPOINT_URL
--- image2ts_pipeline.py
+++ image2ts_pipeline.py
@@ -1,69 +1,82 @@
 import os
 import glob
 import sys
+import rasterio.transform
 from sentinelhub import CRS
 from typing import List
 from stelar_spatiotemporal.preprocessing.preprocessing import combine_npys_into_eopatches, max_partition_size
 from stelar_spatiotemporal.preprocessing.vista_preprocessing import unpack_vista_unzipped
 from stelar_spatiotemporal.preprocessing.timeseries import lai_to_csv_px, lai_to_csv_field
-from stelar_spatiotemporal.lib import load_bbox, get_filesystem
+from stelar_spatiotemporal.lib import load_bbox, get_filesystem, save_bbox
 import argparse
+import rasterio
+from rasterio.io import MemoryFile
+import re
+import datetime as dt
+import numpy as np
+from sentinelhub import BBox, CRS
 
 parser = argparse.ArgumentParser(description='Convert raster images to time series dataset')
    
-def parse_args(args:List[str]) -> dict:
-    pixel = True
-    field = True
 
-    # Check if unkonwn arguments are given
-    for arg in args[1:]:
-        if arg.startswith("-"):
-            if arg not in ["-skippx", "-skipfields"]:
-                print("Unknown argument: {}".format(arg))
-                sys.exit(1)
-
-
-    # Look if pixel or field needs to be skipped
-    if "-skippx" in args:
-        pixel = False
-        args.remove("-skippx")
-    if "-skipfields" in args:
-        field = False
-        args.remove("-skipfields")
-
-    # If skip field no field path is needed
-    error_str = "Usage: python image2ts_pipeline.py <path_to_ras1>,...,<path_to_rasn> <path_to_rhd1>,...<path_to_rhdn> <path_to_out>"
-    if not field:
-        fields_path = None
-    else:
-        # Look if field path is given
-        if len(args) < 5:
-            print(error_str, "<path_to_fields>")
-            sys.exit(1)
-        fields_path = args[4]
-
-    if len(args) < 4:
-        print(error_str)
-        sys.exit(1)
-
-    ras_paths = args[1].split(",")
-    rhd_paths = args[2].split(",")
-    out_path = args[3]
-
-    args_dict = {"ras_paths":ras_paths, 
-                 "rhd_paths":rhd_paths, 
-                 "out_path":out_path, 
-                 "fields_path":fields_path, 
-                 "pixel":pixel, 
-                 "field":field}
-    
-    print("Arguments: {}".format(args_dict))
-    
-    return args_dict
-
-def unpacking(ras_paths:List[str], rhd_paths:List[str], out_path:str):
+def unpack_ras(ras_paths:List[str], rhd_paths:List[str], out_path:str):
     for ras_path, rhd_path in zip(ras_paths, rhd_paths):
         unpack_vista_unzipped(ras_path, rhd_path, out_path, delete_after=False, crs=CRS('32630'))
+
+def unpack_tif(tif_paths:List[str], outdir:str):
+    fs = get_filesystem(input_path)
+    paths = fs.glob(os.path.join(input_path, "*.{}".format(extension)))
+
+    if len(paths) == 0:
+        raise ValueError("No {} files found in the input folder.".format(extension))
+    
+    # 1. Unpack the TIF files and save to npy
+    # TODO package this into a function in stelar spatiotemporal
+    print(f"Unpacking {len(paths)} files...")
+    gbbox = None
+    for path in paths:
+        # Open the raster
+        with fs.open(path, 'rb') as f, MemoryFile(f) as memfile:
+            with memfile.open() as src:
+                bbox = rasterio.transform.array_bounds(src.height, src.width, src.transform)
+                crs = CRS(src.crs.to_string())
+                bbox = BBox(bbox, crs)
+
+                # Check if the bbox is the same for all images
+                if not gbbox:
+                    gbbox = bbox
+                elif bbox != gbbox:
+                    # print(bbox, gbbox)
+                    raise ValueError("Bounding boxes of the images do not match.")
+                
+                # See if there are timestamps in the metadata in format YYYYMMDD
+                timestamps = src.tags().get("TIFFTAG_DATETIME").split(" ") if "TIFFTAG_DATETIME" in src.tags() else None
+
+                if timestamps is None and src.count == 1:
+                    # Check if there is a timestamp in the filename in the format YYYYMMDD
+                    filename = os.path.basename(path)
+                    timestamps = re.search(r"\d{8}", filename)
+                    if timestamps:
+                        timestamps = [timestamps.group()]
+
+                if not timestamps:
+                    raise ValueError("No timestamps found in the metadata or the filename.")
+                
+                # Parse the timestamps
+                timestamps = [dt.datetime.strptime(timestamp, "%Y%m%d") for timestamp in timestamps]
+
+                # Read each of the images and save them to npy
+                for timestamp in timestamps:
+                    img = src.read().squeeze()
+
+                    # Save the image to npy
+                    npy_path = os.path.join(outdir, "{}.npy".format(timestamp.strftime("%Y_%m_%d")))
+                    np.save(npy_path, img)
+
+    # Save the bbox
+    bbox_path = os.path.join(outdir, "bbox.pkl")
+    save_bbox(bbox, bbox_path)
+
 
 def combining_npys(npy_dir:str, out_path:str): 
 
@@ -108,35 +121,19 @@ def cleanup(tmp_path:str):
             print("Deleting {}".format(todel_path))
             os.system("rm -rf {}".format(todel_path))
 
+def check_ras(input_path):
+    # Check if rhd_paths match with ras_paths
 
-def image2ts_pipeline(input_path:str, output_path:str, field_path:str, 
-                      skip_pixel:bool):
-    """
-    This function takes a directory of raster files and headers and converts them to time series dataset.
-
-    Parameters
-    ----------
-    input_path : str
-        Path to the folder containing the input images as RAS and RHD files.
-    output_path : str
-        Path to the folder where the output files will be saved.
-    field_path : str
-        Path to the shapefile containing the boundaries of the agricultural fields. If not given, field-level time series will not be created.
-    skip_pixel : bool
-        Skip creating pixel-level time series
-    """
-    # Get the ras and rhd paths
     fs = get_filesystem(input_path)
     ras_paths = fs.glob(os.path.join(input_path, "*.RAS"))
     rhd_paths = fs.glob(os.path.join(input_path, "*.RHD"))
-
-    # Check if we do pixel, field, or both
-    pixel = not skip_pixel
-    field = field_path is not None
-
-    # Check if rhd_paths match with ras_paths
+    if len(ras_paths) == 0:
+        raise ValueError("No RAS files found in the input folder.")
+    if len(rhd_paths) == 0:
+        raise ValueError("No RHD files found in the input folder.")
     if len(ras_paths) != len(rhd_paths):
-        raise ValueError("Length of ras_paths and rhd_paths should be equal.")
+        raise ValueError("Number of RAS and RHD files do not match.")
+    
     ras_paths_match = []
     rhd_paths_match = []
     for ras_path in ras_paths:
@@ -149,15 +146,61 @@ def image2ts_pipeline(input_path:str, output_path:str, field_path:str,
         else:
             raise ValueError("No matching rhd_path found for ras_path: {}".format(ras_path))
         
-    TMP_PATH = '/tmp'
-        
-    # 1. Unpack the RAS files
-    print("1. Unpacking RAS files...")
-    npy_dir = os.path.join(TMP_PATH, "npys")
-    unpacking(ras_paths=ras_paths_match, 
-              rhd_paths=rhd_paths_match, 
-              out_path=npy_dir)
+    return ras_paths_match, rhd_paths_match
+
+
+def image2ts_pipeline(input_path:str, extension:str,
+                      output_path:str, 
+                      field_path:str, 
+                      skip_pixel:bool):
+    """
+    This function takes a directory of raster files and headers and converts them to time series dataset.
+
+    Parameters
+    ----------
+    input_path : str
+        Path to the folder containing the input images.
+    extension : str
+        Extension of the input files. Default is 'RAS'.
+    output_path : str
+        Path to the folder where the output files will be saved.
+    field_path : str
+        Path to the shapefile containing the boundaries of the agricultural fields. If not given, field-level time series will not be created.
+    skip_pixel : bool
+        Skip creating pixel-level time series
+    """
+    # Check if we do pixel, field, or both
+    pixel = not skip_pixel
+    field = field_path is not None
+
+    # Check if the extension is supported
+    if extension not in ["RAS", "TIF", "TIFF"]:
+        raise ValueError("Extension {} is not supported.".format(extension))
     
+    TMP_PATH = '/tmp'
+    npy_dir = os.path.join(TMP_PATH, "npys")
+    if not os.path.exists(npy_dir):
+        os.makedirs(npy_dir)
+
+    if extension == "RAS":
+        ras_paths, rhd_paths = check_ras(input_path)
+
+        # 1. Unpack the RAS files
+        print("1. Unpacking RAS files...")
+        unpack_ras(ras_paths=ras_paths, 
+                rhd_paths=rhd_paths, 
+                out_path=npy_dir)
+    else:
+        # TODO do not do temporary unpacking for TIF files for field level time series -> directly read the TIF files
+        # 1. Unpack the TIF files
+        print("1. Unpacking {} files...".format(extension))
+        
+        fs = get_filesystem(input_path)
+        tif_paths = fs.glob(os.path.join(input_path, "*.{}".format(extension)))
+
+        unpack_tif(tif_paths=tif_paths,
+                     outdir=npy_dir)
+
     # 2. Combining the images into eopatches
     print("2. Combining the images into eopatches...")
     eopatches_dir = os.path.join(TMP_PATH, "lai_eopatch")
@@ -177,7 +220,8 @@ def image2ts_pipeline(input_path:str, output_path:str, field_path:str,
     if field:
         field_ts_path = os.path.join(output_path, "field_timeseries")
         print("4. Creating field-level time series...")
-        create_field_ts(out_path=field_ts_path, 
+        create_field_ts(eop_dir=eopatches_dir,
+                        out_path=field_ts_path, 
                         fields_path=field_path)
 
     # 5. Cleanup
@@ -188,7 +232,12 @@ def image2ts_pipeline(input_path:str, output_path:str, field_path:str,
 parser.add_argument("-i", "--input_path", 
                     type=str, 
                     required=True,
-                    help="Path to the folder containing the input images as RAS and RHD files.")
+                    help="Path to the folder containing the input images.")
+parser.add_argument("-x", "--file_extension", 
+                    type=str, 
+                    required=False,
+                    default="RAS",
+                    help="Extension of the input files. Default is 'RAS'.")
 parser.add_argument("-o", "--output_path",
                     type=str,
                     required=True,
@@ -219,8 +268,10 @@ parser.add_argument("--MINIO_ENDPOINT_URL",
     
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        input_path = "s3://stelar-spatiotemporal/LAI_small"
+        input_path = "s3://stelar-spatiotemporal/LAI_fused"
         output_path = "s3://stelar-spatiotemporal"
+        extension = 'TIF'
+        # field_path = "s3://stelar-spatiotemporal/fields_2020_07_27.gpkg"
         field_path = None
         skip_pixel = False
         MINIO_ACCESS_KEY = "minioadmin"
@@ -229,6 +280,7 @@ if __name__ == "__main__":
     else:
         args = parser.parse_args()
         input_path = args.input_path
+        extension = args.file_extension
         output_path = args.output_path
         field_path = args.field_path
         skip_pixel = args.skip_pixel
@@ -249,6 +301,7 @@ if __name__ == "__main__":
         os.environ["MINIO_ENDPOINT_URL"] = MINIO_ENDPOINT_URL
     
     image2ts_pipeline(input_path=input_path,
+                      extension=extension,
                       output_path=output_path,
                       field_path=field_path,
                       skip_pixel=skip_pixel)
